c541dca037250aadb9f2a14b016ba00daf53ecb2
Sabhyaa <pradhansabhyaa2005@gmail.com>
2024-03-02 18:43:21 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-
-const About = React.lazy(() => import("./About"));
-const Tech = React.lazy(() => import("./Tech"));
-const Works = React.lazy(() => import("./Works"));
-const Contact = React.lazy(() => import("./Contact"));
-const StarsCanvas = React.lazy(() => import("./StarsCanvas"));
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from '../src/components'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/tech" component={Tech} />
-            <Route path="/works" component={Works} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-          <div className="relative z-0">
-            <Suspense fallback={<div>Loading...</div>}>
-              <StarsCanvas />
-            </Suspense>
-          </div>
-        </Suspense>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
       </div>
+      <div>
+        <About />
+        <Tech />
+        <Works />
+      </div>  
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
